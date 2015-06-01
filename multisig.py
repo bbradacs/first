0c8892efcff855a4c74fa97704c6b662c10db2b6
bbradacs <bbradacs@gmail.com>
2015-06-01 10:06:36 -0400

    a bit more development

--- multisig.py
+++ multisig.py
@@ -3,7 +3,7 @@ import pybitcointools as bitcoin
 
 # send BTC here!
 fromAddress = '1AG6FXCHJSdtZtvsfwZYo6dVzGNV9gzDqG'
-amount = 0.1
+amount = 0.01
 fee = 0.0
 
 # In our case MultiSig works with three parties.  Each party keeps
@@ -19,12 +19,30 @@ pubkey1 = bitcoin.privtopub(privkey1)
 pubkey2 = bitcoin.privtopub(privkey2)
 pubkey3 = bitcoin.privtopub(privkey3)
 
+print 'privkey1'
+print '    ' + repr(privkey1)
+print 'privkey2'
+print '    ' + repr(privkey2)
+print 'privkey3'
+print '    ' + repr(privkey3)
+print ' ' 
+
+print 'pubkey1'
+print '    ' + repr(pubkey1)
+print 'pubkey2'
+print '    ' + repr(pubkey2)
+print 'pubkey3'
+print '    ' + repr(pubkey3) 
+print ' '
+
 # Make a list of the public keys
 pub = []
 for x in range(0,3):
     pub.append(pubkey1)
-print 'pub'
-print '    ' + repr(pub)
+
+print 'len(pub)'
+print '    ' + repr(len(pub))
+print ' ' 
 
 # 
 script = bitcoin.mk_multisig_script(pub[0], pub[1], pub[2], 2, 3)
@@ -32,8 +50,26 @@ address = bitcoin.scriptaddr(script)
 
 print 'script'
 print '    ' + script
-print 'address'
+print '(script) address'
 print '    ' + address
+
+# just for kicks, see if the order of the keys matter when generating
+# a script address; as far as I can tell, order does not matter
+script1 = bitcoin.mk_multisig_script(pub[2], pub[0], pub[1], 2, 3)
+address1 = bitcoin.scriptaddr(script)
+print 'script2'
+print '    ' + script1
+print 'address1'
+print '    ' + address1
+print ' '
+
+# are they equal?
+if script == script1:
+    print 'the two scripts are the same'
+else:
+    print ' the two scripts are different'
+
+print ' ' 
 
 # figure out how much bitcoin is available at this address
 history = bitcoin.unspent(fromAddress)
